e0b88bae3322dd846282c2a4fce496cc3bd4781a
cmartins2001 <cmartinspfhs2001@gmail.com>
2024-12-05 10:08:36 -0500

    started day 2

---
+++ day 2/day2.py
@@ -0,0 +1,14 @@
+'''
+Advent of Code
+Day 2
+'''
+
+##### PART 1 #####
+
+# Initialize safe report counter:
+safe_report_count = 0
+
+# Read the input file:
+with open('day1_input.txt', 'r') as f:
+
+    for line in f.readlines():
