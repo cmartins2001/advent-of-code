e8858d6e48bb742a3654af653acd48ada1427136
Connor Martins <cmartins61@massmutual.com>
2024-12-05 10:34:35 -0500

    worked on day 2

--- day 2/day2.py
+++ day 2/day2.py
@@ -9,6 +9,22 @@ Day 2
 safe_report_count = 0
 
 # Read the input file:
-with open('day1_input.txt', 'r') as f:
+with open('day2_input.txt', 'r') as f:
 
     for line in f.readlines():
+
+        # Extract the five numbers:
+        num_list = line.split(' ')
+
+        # Convert them to integer types:
+        int_list = [int(x) for x in num_list]
+
+        # Check work:
+        # print(int_list)
+
+        # Check 1 - all increasing?
+        loop1 = True
+        for i in range(len(int_list)):
+            while int_list[i+1] > int_list[i]:
+                continue
+            
